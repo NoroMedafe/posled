85695d3acea08ee789268f937c028e185d986a9e
NoroMedafe <danya1515@inbox.ru>
2022-04-08 14:26:49 +0300

    Добавьте файлы проекта.

---
+++ posled/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace posled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int basicNumber = 5;
+            int finishNumber = 96;
+            int step = 7;
+
+            while (basicNumber <= finishNumber)
+            {
+                Console.Write(basicNumber + " ");
+                basicNumber += step;
+            } 
+
+            Console.ReadKey();
+        }
+    }
+}
